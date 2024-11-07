887053493a487cb567997c213f88a71bf4035241
noblechap <40839815+noblechap@users.noreply.github.com>
2024-11-07 08:31:54 -0500

    Add E91 constants

---
+++ e91-constants.ts
@@ -0,0 +1,12 @@
+// E91 Web Socket Event Constants
+export const A_MEASURE_EVENT = 'A_MEASURE';
+export const B_MEASURE_EVENT = 'B_MEASURE';
+export const A_PREFERENCE_EVENT = 'A_PREFERENCE';
+export const B_PREFERENCE_EVENT = 'B_PREFERENCE';
+export const A_DICE_EVENT = 'A_DICE';
+export const B_DICE_EVENT = 'B_DICE';
+export const VALIDATION_INDICES_EVENT = 'VALIDATION_INDICES';
+export const A_BITS_EVENT = 'A_BITS';
+export const B_BITS_EVENT = 'B_BITS';
+export const A_DECISION_EVENT = 'A_DECISION';
+export const B_DECISION_EVENT = 'B_DECISION';
